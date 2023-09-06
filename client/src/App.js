a28896fec5f1c39a088dbf7447a6a7dd140764fb
augusto.izepon <augustom.izepon@gmail.com>
2023-09-06 16:27:08 -0300

    Trabalho terminado, agora faltam apenas detalhes

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import axios from "axios";
 
 function App() {
+  // Estado para armazenar os valores dos campos de entrada
   const [values, setValues] = useState(
     {
       codigo: '',
@@ -10,6 +11,15 @@ function App() {
     }
   )
 
+  // Estado para armazenar a lista de produtos
+  const [productList, setProductList] = useState([]);
+  // Estado para armazenar a lista de pacotes
+  const [packList, setPackList] = useState([])
+
+  // Estado para controlar a habilitação do botão de atualização
+  const [isUpdateButtonDisabled, setIsUpdateButtonDisabled] = useState(true);
+
+  // Função para lidar com a alteração dos valores dos campos de entrada
   const handleChangeValues = value => {
     setValues(preValue => ({
       ...preValue,
@@ -17,23 +27,78 @@ function App() {
     }))
   }
 
+  // Função para validar se a atualização do produto é possível
+  const validationBtn = async () => {
+    let update = {
+      new_price: Number(values.valorNovo),
+      product_code: Number(values.codigo)
+    };
+    const jsonData = await axios.get(`http://localhost:3001/`);
+    const productByCode = jsonData.data.find(product => product.code === update.product_code);
+  
+    // Verifica se a atualização é válida com base em algumas condições
+    const isValid = (
+      productByCode &&
+      update.new_price &&
+      (update.new_price - productByCode.sales_price) <= productByCode.sales_price * 0.1 &&
+      update.new_price > productByCode.sales_price
+    );
+  
+    // Desabilita o botão de atualização se não for válido
+    setIsUpdateButtonDisabled(!isValid);
+  }
 
-  const handleClickButton = async () => {
+  // Função para realizar a atualização do preço do produto
+  const updateBtn = async () =>{
     let update = {
       new_price: Number(values.valorNovo),
       product_code: Number(values.codigo)
     }
     const jsonData = await axios.get(`http://localhost:3001/`)
-    const productByCode = jsonData.data.find(product => product.code === update.product_code)
-    if(update.new_price && (update.new_price - productByCode.sales_price) <= productByCode.sales_price * 0.1 && update.new_price > productByCode.sales_price){
-      await axios.put(`http://localhost:3001/`, update)
-      console.log(values)
-    } else{
-      alert("O valor só pode ser de ate 10% a mais")
-    }
-    
+    jsonData.data.find(product => product.code === update.product_code)
+    await axios.put(`http://localhost:3001/`, update)
   }
-  //de acordo com o codigo do produto, apos acionar o botao procurar, vai verificar no arquivo json se consta, para entao poder modificar
+
+  // Função para atualizar a lista de produtos
+  const refreshProducts = async () => {
+    try {
+      const response = await axios.get(`http://localhost:3001/`);
+      setProductList(response.data);
+    } catch (error) {
+      console.error("Erro ao buscar a lista de produtos: ", error);
+    }
+  };
+
+  // Função para atualizar a lista de pacotes
+  const refreshPacks = async () => {
+    try {
+      const responsePacks = await axios.get(`http://localhost:3001/packs`);
+      const packData = responsePacks.data
+
+      let newPacklist = []
+      for (let i = 0; i < packData.length; i++) {
+        const pack = packData[i]
+        const product = productList.find(product => product.code === pack.product_id)
+        const newPack = {
+          pack: pack,
+          product: product
+        }
+        newPacklist.push(newPack)
+      }
+      setPackList(newPacklist)
+    } catch (error) {
+      console.error("Erro ao buscar a lista de produtos: ", error);
+    }
+  }
+
+  // Variável para exibir alerta se o botão de atualização estiver desabilitado
+  let alert = ``
+
+  if (isUpdateButtonDisabled === true){
+    alert = `Por Favor, preencha os dados corretamente, o novo valor do produto não ultrapassa 10% do seu valor atual`
+  }
+
+  // Função para buscar um produto pelo código
   const searchClickBtn = async value => {
     setValues(preValue => ({
       ...preValue,
@@ -51,30 +116,83 @@ function App() {
           currency: 'BRL',
         })
       });
-
-    }else {
+    } else {
       setValues({
-        nomeProduto: `produto nao encontrado`
+        nomeProduto: `(404) Produto não encontrado. Por favor, verifique a lista de produtos e tente novamente`
       })
     }
   }
 
   return (
     <div className="app--container">
-      <div className="register--container">
-        <h1>Validacao de produto</h1>
-        <label className="product-code--label">codigo do produto:</label>
-        <input name="codigo"type="number"  className="product-code--input" inputmode="numeric" onChange={handleChangeValues}></input>
-        <button className="search--btn" onClick={searchClickBtn} >Procurar</button>
-        <label className="product-name--label">nome do produto:</label>
-        <p name="nome" className="product-name--p">{values.nomeProduto}</p>
-        <label className="product-first-value--label">valor atual do produto:</label>
-        <p className="product-first-value--p">{values.valorAtual}</p>
-        <label className="product-second-value--label">novo valor do produto:</label>
-        <input name="valorNovo" type="number" className="product-second-value--input" inputmode="numeric" onChange={handleChangeValues}></input>
-        <button className="validation--btn" onClick={handleClickButton}>Validar/Atualizar</button>
+      <img src="https://landing.shopper.com.br/static/media/logo-original.c7089ad32bcf61645d35.webp" alt="logo-shopper"></img>
+      <div className="main--container">
+        <div className="product--container">
+          <h2>Lista de produtos</h2>
+          <ol className="product-list--container">
+            {productList.map((product) => (
+              <li key={product.code}>
+                <table>
+                  <tr>
+                    <th className="code--table">codigo</th>
+                    <th className="name--table">nome</th>
+                    <th className="price--table">preco</th>
+                  </tr>
+                  <tr>
+                    <td className="code--table">{product.code}</td>
+                    <td className="name--table">{product.name}</td>
+                    <td className="price--table">{product.sales_price.toLocaleString('pt-BR', {
+                      style: 'currency',
+                      currency: 'BRL',
+                    })}</td>
+                  </tr>
+                </table>
+              </li>
+            ))}
+          </ol>
+          <button className="updateStatus-product--btn" onClick={refreshProducts}>Atualizar Lista</button>
+        </div>
+        <div className="packs--container">
+          <h2>Lista de packs</h2>
+          <ol className="product-list--container">
+            {packList.map((pack) => (
+              <li key={pack.code}>
+                <table>
+                  <tr>
+                    <th className="code-pack--table">cod. pack</th>
+                    <th className="description-pack--table">descricao</th>
+                    <th className="price-pack-table">preco</th>
+                  </tr>
+                  <tr>
+                    <td className="code--table">{pack.pack.pack_id}</td>
+                    <td className="name--table">{pack.pack.qty} x {pack.product.name}</td>
+                    <td className="price-pack--table">{(pack.product.sales_price * pack.pack.qty).toLocaleString('pt-BR', {
+                      style: 'currency',
+                      currency: 'BRL',
+                    })}</td>
+                  </tr>
+                </table>
+              </li>
+            ))}
+          </ol>
+          <button className="updateStatus-pack--btn" onClick={refreshPacks}>Atualizar Lista</button>
+        </div>
+        <div className="register--container">
+          <h2>Validação de produto</h2>
+          <label className="product-code--label">codigo do produto:*</label>
+          <input name="codigo" type="number" className="product-code--input" inputmode="numeric" onChange={handleChangeValues}></input>
+          <button className="search--btn" onClick={searchClickBtn} >Procurar</button>
+          <label className="product-name--label">nome do produto:</label>
+          <p name="nome" className="product-name--p">{values.nomeProduto}</p>
+          <label className="product-first-value--label">valor atual do produto:</label>
+          <p className="product-first-value--p">{values.valorAtual}</p>
+          <label className="product-second-value--label">novo valor do produto:*</label>
+          <input name="valorNovo" type="number" className="product-second-value--input" inputmode="numeric" onChange={handleChangeValues}></input>
+          <button className="validation--btn" onClick={validationBtn}>Validar</button>
+          <button className="update--btn" onClick={updateBtn} disabled={isUpdateButtonDisabled}>Atualizar Produto</button>
+          <p className="alert--p">{alert}</p>
+        </div>
       </div>
-
     </div>
   );
 }
--- server/controllers/products.js
+++ server/controllers/products.js
@@ -1,48 +1,93 @@
-const mysql = require(`mysql`)
-
+// Importar as bibliotecas necessárias
+const mysql = require('mysql');
+const fs = require('fs/promises');
+const { path } = require('../routers/routes');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const { format } = require(`date-fns-tz`)
+let changes = []
+// Configuração da conexão com o banco de dados MySQL
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "12345678",
     database: "database"
-})
+});
 
+// Função para obter os produtos do banco de dados
 const getProducts = (req, res) => {
-    let sql = "SELECT * FROM products"
+    let sql = "SELECT * FROM products";
+
+    // Executar a consulta SQL e enviar os resultados como resposta
     db.query(sql, async (err, result) => {
-        res.send(result)
-    })
+        res.send(result);
+    });
 }
 
-
+// Função para atualizar os dados de um produto
 const updateProductData = (req, res) => {
-    const {product_code, new_price} = req.body
-    console.log(new_price)
+    const { product_code, new_price } = req.body;
+    const date = new Date()
+    const dateFormated = format(date, 'dd/MM/yy HH:mm:ss')
+
+    const changeObject = {
+        product_code: product_code,
+        new_price: new_price,
+        date: dateFormated
+    }
+
+    changes.push(changeObject)
+
+    const writePriceChangesCsv = (changes) => {
+        const csvWriter = createCsvWriter({
+            path: './database/csv/changes.csv',
+            header: [
+                { id: 'product_code', title: 'Código do Produto' },
+                { id: 'new_price', title: 'Novo Preço' },
+                { id: 'date', title: 'Data' },
+            ]
+        })
+    
+        csvWriter.writeRecords(changes)
+        .then(() => {
+            console.log('Arquivo CSV criado com sucesso.');
+        })
+        .catch((error) => {
+            console.error('Erro ao criar o arquivo CSV:', error);
+        });
+    }
+        writePriceChangesCsv(changes)
+
+
+    // Criar um objeto com os dados atualizados do produto
     const updatedProductData = {
         sales_price: Number(new_price),
     };
 
+    // Consulta SQL para atualizar os dados do produto com base no código
     const sql = "UPDATE products SET ? WHERE code = ?";
     db.query(sql, [updatedProductData, product_code], async (err, result) => {
         if (err) {
             console.error(err);
             res.status(500).json({ error: "Erro ao atualizar o produto" });
         } else {
-            console.log(`Produto com Codigo ${product_code} atualizado com sucesso`);
+            console.log(`Produto com Código ${product_code} atualizado com sucesso`);
             res.json({ message: "Produto atualizado com sucesso" });
         }
     });
 }
 
+// Função para obter os pacotes do banco de dados
 const getPacks = (req, res) => {
-    let sql = "SELECT * FROM packs"
+    let sql = "SELECT * FROM packs";
+
+    // Executar a consulta SQL e enviar os resultados como resposta
     db.query(sql, async (err, result) => {
-        const results = JSON.stringify(result)
-        res.send(result)
-    })
+        const results = JSON.stringify(result);
+        res.send(result);
+    });
 }
 
-
+// Exportar as funções para serem usadas em outros lugares
 module.exports = {
     getProducts,
     getPacks,
